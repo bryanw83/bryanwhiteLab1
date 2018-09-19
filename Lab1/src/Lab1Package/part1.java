29546dc0855c0aed4ac6daa085d690ad5dded591
bryan <bryanw83@yahoo.com>
2018-09-19 09:30:55 -0400

    First commit for Lab1

---
+++ Lab1/src/Lab1Package/part1.java
@@ -0,0 +1,60 @@
+package Lab1Package;
+import java.util.Scanner;
+
+public class part1 {
+	public static void main(String [] args) {
+		Scanner scnr = new Scanner(System.in);
+
+		
+//Here we have the initial set up and prompt to receive our variables		
+		int userNum1;
+		int userNum2;
+		
+		System.out.println("Please enter your first 3-digit number and press 'Enter'.");
+			userNum1 = scnr.nextInt();
+			
+		System.out.println("Please enter your second 3-digit number and press 'Enter'.");
+			userNum2 = scnr.nextInt();
+	
+	
+			
+			
+			
+/*Next we isolate the individual values of each variable into new separate 			
+variables using modulus*/	
+			
+			int num1a = userNum1 / 100 % 10;
+			int num2a = userNum1 / 10 % 10;
+			int num3a = userNum1 % 10;
+			
+			int num1b = userNum2 / 100 % 10;
+			int num2b = userNum2 / 10 % 10;
+			int num3b = userNum2 % 10;
+			
+			
+			
+			
+//Next we perform the following equations in order to get our final variables			
+			
+			
+			int finNum1 = num1a + num1b;
+			int finNum2 = num2a + num2b;
+			int finNum3 = num3a + num3b;
+			
+			
+			
+			
+//Finally, we use an if/else statement to compare our final variables			
+			
+			
+			if ((finNum1 == finNum2) && (finNum2 == finNum3) && (finNum1 == finNum3)) {
+				System.out.println("True");
+			}
+			else {
+				System.out.println("False");
+			}
+
+
+scnr.close();
+	}
+}
---
+++ Lab1/src/Lab1Package/part2.java
@@ -0,0 +1,48 @@
+package Lab1Package;
+import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.time.Period;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.regex.MatchResult;
+
+public class part2 {
+
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+		
+		String input1;
+		String input2;
+		
+		System.out.println("Please enter a date as MM/DD/YYYY");
+		input1 = scnr.nextLine();
+		
+		System.out.println("Please enter a seccond date as MM/DD/YYYY");
+		input2 = scnr.nextLine();
+		
+
+		
+		//SimpleDateFormat simpledf = new SimpleDateFormat("mm-dd-yyyy");
+	       //String stringDate = "";
+	       //Date newDate = simpledf.parse(stringDate);
+	       //System.out.println(newDate); 
+		
+		
+		
+		//LocalDate endofCentury = LocalDate.of(2014, 01, 01);
+		//LocalDate now = LocalDate.now();
+		 
+		//Period diff = Period.between(endofCentury, now);
+		 
+		//System.out.printf("Difference is %d years, %d months and %d days old",
+		                    //diff.getYears(), diff.getMonths(), diff.getDays());		
+		
+		
+		
+		
+		
+		
+	scnr.close();	
+	}
+
+}
