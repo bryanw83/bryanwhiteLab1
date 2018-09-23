5a83a015bd4cc999ce23a7ff3e7b2ae32a33e645
bryan <bryanw83@yahoo.com>
2018-09-23 17:35:17 -0400

    Final Lab Submission

--- Lab1/src/Lab1Package/part1.java
+++ Lab1/src/Lab1Package/part1.java
@@ -22,32 +22,32 @@ public class Part1 {
 /*Next we isolate the individual values of each variable into new separate 			
 variables using modulus*/	
 			
-			int num1a = userNum1 / 100 % 10;
-			int num2a = userNum1 / 10 % 10;
-			int num3a = userNum1 % 10;
+		int num1a = userNum1 / 100 % 10;
+		int num2a = userNum1 / 10 % 10;
+		int num3a = userNum1 % 10;
 			
-			int num1b = userNum2 / 100 % 10;
-			int num2b = userNum2 / 10 % 10;
-			int num3b = userNum2 % 10;
+		int num1b = userNum2 / 100 % 10;
+		int num2b = userNum2 / 10 % 10;
+		int num3b = userNum2 % 10;
 			
 			
 			
 			
 //Next we perform the following equations in order to get our final variables			
 			
-			int finNum1 = num1a + num1b;
-			int finNum2 = num2a + num2b;
-			int finNum3 = num3a + num3b;
+		int finNum1 = num1a + num1b;
+		int finNum2 = num2a + num2b;
+		int finNum3 = num3a + num3b;
 			
 			
 			
 			
 //Finally, we use an if/else statement to compare our final variables			
 			
-			if ((finNum1 == finNum2) && (finNum2 == finNum3) && (finNum1 == finNum3)) {
+		if ((finNum1 == finNum2) && (finNum2 == finNum3) && (finNum1 == finNum3)) {
 				System.out.println("True");
 			}
-			else {
+		else {
 				System.out.println("False");
 			}
 
