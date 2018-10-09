8027e147ccde62dd68f0e6483505411c655c906c
bryan <bryanw83@yahoo.com>
2018-10-09 14:22:32 -0400

    Added a line

---
+++ TechHireBootcamp/src/HelloWorld/HelloWorld.java
@@ -0,0 +1,13 @@
+package HelloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Hello World");
+		
+		System.out.println("I LOVE JAVA!!!");
+
+	}
+
+}
